19581dec815b919f29add42158cde17024e27fd0
Jared Taylor <jaredtaylor.99@gmail.com>
2024-10-20 13:37:18 +1300

    Add editor module to hide forbidden properties

---
+++ Source/PushPawnEditor/Private/PushPawnAbilityDetailsCustomization.h
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Jared Taylor. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "IDetailCustomization.h"
+
+/**
+ * 
+ */
+class FPushPawnAbilityDetailsCustomization : public IDetailCustomization
+{
+public:
+	static TSharedRef<IDetailCustomization> MakeInstance();
+
+	virtual void CustomizeDetails(IDetailLayoutBuilder& DetailBuilder) override;
+};
