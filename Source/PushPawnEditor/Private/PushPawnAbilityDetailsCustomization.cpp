f01ec494706bf4b9f8be0da0b29ce18f22e78708
Jared Taylor <jaredtaylor.99@gmail.com>
2024-10-20 13:37:18 +1300

    Add editor module to hide forbidden properties

---
+++ Source/PushPawnEditor/Private/PushPawnAbilityDetailsCustomization.cpp
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Jared Taylor. All Rights Reserved
+
+
+#include "PushPawnAbilityDetailsCustomization.h"
+
+#include "DetailLayoutBuilder.h"
+#include "Abilities/PushPawn_Ability.h"
+
+
+TSharedRef<IDetailCustomization> FPushPawnAbilityDetailsCustomization::MakeInstance()
+{
+	return MakeShared<FPushPawnAbilityDetailsCustomization>();
+}
+
+void FPushPawnAbilityDetailsCustomization::CustomizeDetails(IDetailLayoutBuilder& DetailBuilder)
+{
+	TArray<TWeakObjectPtr<UObject>> Objects;
+	DetailBuilder.GetObjectsBeingCustomized(Objects);
+
+	for (const TWeakObjectPtr<UObject>& Object : Objects)
+	{
+		if (UPushPawn_Ability* PushPawnAbility = Cast<UPushPawn_Ability>(Object.Get()))
+		{
+			// Bump PushPawn to the top of the details panel
+			DetailBuilder.EditCategory(TEXT("PushPawn"), FText::GetEmpty(), ECategoryPriority::Transform);
+
+			// We use a lightweight CanActivateAbility(), so we can remove unavailable tags
+			DetailBuilder.HideProperty("ActivationRequiredTags", UGameplayAbility::StaticClass());
+			DetailBuilder.HideProperty("ActivationBlockedTags", UGameplayAbility::StaticClass());
+			DetailBuilder.HideProperty("SourceRequiredTags", UGameplayAbility::StaticClass());
+			DetailBuilder.HideProperty("SourceBlockedTags", UGameplayAbility::StaticClass());
+			DetailBuilder.HideProperty("TargetRequiredTags", UGameplayAbility::StaticClass());
+			DetailBuilder.HideProperty("TargetBlockedTags", UGameplayAbility::StaticClass());
+
+			// Unavailable properties
+			DetailBuilder.HideProperty("CostGameplayEffectClass", UGameplayAbility::StaticClass());
+			DetailBuilder.HideProperty("CooldownGameplayEffectClass", UGameplayAbility::StaticClass());
+
+			// Irrelevant properties (handled internally)
+			DetailBuilder.HideProperty("AbilityTriggers", UGameplayAbility::StaticClass());
+
+			// Avoid designers causing astronomical bandwidth costs
+			DetailBuilder.HideProperty("bReplicateInputDirectly", UGameplayAbility::StaticClass());
+			DetailBuilder.HideProperty("ReplicationPolicy", UGameplayAbility::StaticClass());
+		}
+	}
+}
---
+++ Source/PushPawnEditor/PushPawnEditor.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class PushPawnEditor : ModuleRules
+{
+    public PushPawnEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "PushPawn",
+                "GameplayAbilities",
+            }
+        );
+    }
+}
